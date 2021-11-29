7847647d8248ae718c6e516b9d4de3765940edd4
Cihan <cihansakaryacs@gmail.com>
2021-11-29 03:36:46 +0300

    feat: Update state machine systems

--- Source/OneVOneCombat/Private/InputQueueSystem.cpp
+++ Source/OneVOneCombat/Private/InputQueueSystem.cpp
@@ -34,59 +34,74 @@ void UInputQueueSystem::ConsumeInputs(UPlayerInputPollingSystem* inputPollingSys
 
 	for (const auto& inputQueueDataAsset : inputQueueDataAssets)
 	{
+		// TODO: Don't add to candidates if last input is not equal
 		if (inputQueueDataAsset->GetInputActions().Num() <= currentInputPoll.Num())
 		{
 			inputQueueDataCandidates.Add(inputQueueDataAsset);
 		}
 	}
 
+	if (inputQueueDataCandidates.IsEmpty())
+	{
+		return;
+	}
+
 	for (int32 i = 0; i < currentInputPoll.Num(); ++i)
 	{
 		const FUserInput& currentUserInput = currentInputPoll[i];
 
-		float lastTwoInputInterval = 0.f;
-		float nextTwoInputInterval = 0.f;
+		int32 lastTwoInputInterval = 0;
+		int32 nextTwoInputInterval = 0;
 
 		if (currentInputPoll.Num() > 1)
 		{
 			if (i > 0)
 			{
 				const FUserInput& beforeCurrentUserInput = currentInputPoll[i - 1];
-				lastTwoInputInterval = currentUserInput.timeStamp - beforeCurrentUserInput.timeStamp;
+				lastTwoInputInterval = FMath::Max(currentUserInput.timeStamp - beforeCurrentUserInput.timeStamp, 0);
 			}
 
 			if (i < currentInputPoll.Num() - 1)
 			{
 				const FUserInput& afterCurrentUserInput = currentInputPoll[i + 1];
-				nextTwoInputInterval = currentUserInput.timeStamp - afterCurrentUserInput.timeStamp;
+				nextTwoInputInterval = FMath::Max(currentUserInput.timeStamp - afterCurrentUserInput.timeStamp, 0);
 			}
 		}
 
-		for (int32 inputQueueIndex = 0; inputQueueIndex < inputQueueDataCandidates.Num(); ++i)
+		for (int32 inputQueueIndex = 0; inputQueueIndex < inputQueueDataCandidates.Num(); ++inputQueueIndex)
 		{
 			const auto* const inputQueueData = inputQueueDataCandidates[inputQueueIndex];
 			const auto& inputActions = inputQueueData->GetInputActions();
+
+			if (i >= inputActions.Num())
+			{
+				continue;
+			}
+
 			const int32 actionIndex = inputActions.Num() - i - 1;
 			const FInputQueueAction& currentQueueAction = inputActions[actionIndex];
 
 			if (currentQueueAction.inputType != currentUserInput.inputType ||
 				currentQueueAction.inputEvent != currentUserInput.inputEvent)
 			{
-				inputQueueDataCandidates.RemoveAt(inputQueueIndex--);
+				inputQueueDataCandidates.RemoveAt(inputQueueIndex);
+				inputQueueIndex > 0 && --inputQueueIndex;
 				continue;
 			}
 
 			if (currentQueueAction.minPreviousInputTime > lastTwoInputInterval ||
 				currentQueueAction.maxPreviousInputTime < lastTwoInputInterval)
 			{
-				inputQueueDataCandidates.RemoveAt(inputQueueIndex--);
+				inputQueueDataCandidates.RemoveAt(inputQueueIndex);
+				inputQueueIndex > 0 && --inputQueueIndex;
 				continue;
 			}
 
 			if (currentQueueAction.minNextInputTime > nextTwoInputInterval ||
 				currentQueueAction.maxNextInputTime < nextTwoInputInterval)
 			{
-				inputQueueDataCandidates.RemoveAt(inputQueueIndex--);
+				inputQueueDataCandidates.RemoveAt(inputQueueIndex);
+				inputQueueIndex > 0 && --inputQueueIndex;
 				continue;
 			}
 		}
@@ -104,5 +119,5 @@ void UInputQueueSystem::ConsumeInputs(UPlayerInputPollingSystem* inputPollingSys
 
 	inputPollingSystem->RemoveFromPolling(currentInputPoll.Num()); // TODO: Remove input from poll if expired
 
-	LOG_TO_SCREEN_STR("Current Action is %s", EditorUtilities::EnumToString(TEXT("EInputQueueOutputState"), static_cast<uint8>(inputQueueDataCandidates[0]->GetInputQueueOutputState())));
+	LOG_TO_SCREEN_STR("Current Action is {0}", EditorUtilities::EnumToString(TEXT("EInputQueueOutputState"), static_cast<uint8>(inputQueueDataCandidates[0]->GetInputQueueOutputState())));
 }
